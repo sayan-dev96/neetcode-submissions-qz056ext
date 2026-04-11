67c7619cfd44ffb5008e74fae16c7b65b60f9198
Sayan Chatterjee <98595130+sayan-dev96@users.noreply.github.com>
2026-04-11 18:40:24 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alphabet_str = ""
+
+        for char in s:
+            alphabet_str += char if char.isalnum() else ""
+        
+        alphabet_str = alphabet_str.lower()
+        str_reversed = "".join(reversed(alphabet_str))
+
+        return alphabet_str == str_reversed
+
+        
